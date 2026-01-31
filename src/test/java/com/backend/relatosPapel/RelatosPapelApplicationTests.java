3f379cb7c22d0e75ee52ccaf02e3819b778ee032
Fredy Cortes <fredy.cortes808@comunidadunir.net>
2026-01-31 16:12:38 -0500

    scaffold spring initializr

---
+++ src/test/java/com/backend/relatosPapel/RelatosPapelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.relatosPapel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelatosPapelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
